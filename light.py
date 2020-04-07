423d0b27c3bbbcb9afbeceaec77ef0320e16e8a5
Nate Wilson <46329080+natekwilson@users.noreply.github.com>
2020-04-07 16:29:49 -0600

    Update light.py

--- light.py
+++ light.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(25,GPIO.IN)
+GPIO.setup(2,GPIO.IN)
 
 while True:
-	print(GPIO.input(25))
+	print("External: " + GPIO.input(25) + " Internal: " + GPIO.input(2))
