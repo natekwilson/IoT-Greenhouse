b5e29126e499147b7f4ffed931fa808c64af52a6
Nate Wilson <46329080+natekwilson@users.noreply.github.com>
2020-04-13 01:04:55 -0600

    Update pi-sensors.py

--- pi-sensors.py
+++ pi-sensors.py
@@ -17,7 +17,7 @@ def takeSnapShot():
     #Take Image if it's the beginning of a 30minute interval
     if imageCounter == 1:
         imgPathString = (t + ".jpg")
-        serverImagePath = ("greenhouse-logs/images/" + t + ".jpg")
+        serverImagePath = ("public/images/" + t + ".jpg")
         captureCMD = "raspistill -q 10 -o /home/pi/greenhouse-logs/images/%s" % (imgPathString)
         sendCMD = "scp /home/pi/greenhouse-logs/images/%s ec2-user@iotgreenhouse.natewilsonit.com:IoT-Greenhouse/%s" %  (imgPathString,serverImagePath)
         os.system(captureCMD)
@@ -45,7 +45,7 @@ def takeSnapShot():
     dataDict['moisture'] = GPIO.input(moisture)
     dataDict['intLight'] = GPIO.input(internalLight)
     dataDict['extLight'] =GPIO.input(externalLight)
-    dataDict['imgPath'] = serverImagePath
+    dataDict['imgPath'] = serv
     dataDict['title'] = title
     
     #PRINT DATADict
