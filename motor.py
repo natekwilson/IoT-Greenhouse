e0532cfbcd0135044663d7810af353b3e83e95f5
Nate Wilson <46329080+natekwilson@users.noreply.github.com>
2020-04-12 21:05:20 -0600

    Update motor.py

--- motor.py
+++ motor.py
@@ -14,13 +14,12 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(STBY, GPIO.OUT)
 GPIO.setup(AIN1, GPIO.OUT)
 GPIO.setup(AIN2, GPIO.OUT)
-GPIO.setup(PMWA, GPIO.OUT)
+GPIO.setup(PWMA, GPIO.OUT)
 
 GPIO.output(STBY, GPIO.HIGH)
 GPIO.output(AIN1, GPIO.HIGH)
 GPIO.output(AIN2, GPIO.LOW)
-
-GPIO.PWM(PMWA, MotorSpeed)
+GPIO.output(PWMA, GPIO.HIGH)
  
 # infinite loop
 while True:
